072eb3570ce44f28293b23a09555662df28c61a2
naimekattor <naimekattor@gmail.com>
2025-09-27 15:24:25 +0600

    fix seo

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -83,7 +83,6 @@ export default function Header() {
             width={200}
             height={86}
             alt="conten Kuche & raum logo"
-            loading="lazy"
             className="w-auto h-auto"
           />
           <span className="sr-only">Go to homepage</span>
